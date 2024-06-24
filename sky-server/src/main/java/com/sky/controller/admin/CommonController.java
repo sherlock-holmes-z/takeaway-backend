31a61b434c0aed9f1baff08227604e2aff694e4f
sherlock <1105026730@qq.com>
2024-06-24 19:56:27 +0800

    文件上传

--- sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -2,11 +2,15 @@ package com.sky.controller.admin;
 
 import com.sky.result.Result;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Objects;
 
 /**
  * CommonController
@@ -18,10 +22,20 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/admin/common")
 public class CommonController {
 
+    private static final String uploadFilePath = System.getProperty("user.dir") + File.separator + "upload" + File.separator;
+
     @PostMapping("/upload")
     @ConditionalOnMissingBean
-    public Result<String> upload(MultipartFile file){
-        // todo upload
+    public Result<String> upload(MultipartFile multipartFile) throws IOException {
+        System.out.println("1");
+        byte[] bytes = multipartFile.getBytes();
+        String filename = multipartFile.getOriginalFilename();
+        filename = Objects.requireNonNull(filename).substring(filename.lastIndexOf(".") + 1);
+        File file = new File(uploadFilePath + filename);
+        try (FileOutputStream fileOutputStream = new FileOutputStream(file)) {
+            fileOutputStream.write(bytes);
+        }
         return Result.success();
     }
+
 }
